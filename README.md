baaa14edce6085cb942ad0a223c1d2e4f6c4942a
Zong Nan Jin <jinzongnan@gmail.com>
2019-10-12 18:39:04 +0800

    add blog demo code, get from https://flask.palletsprojects.com

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Flask_Demo
-Flask Demo Code
+## Environment:
+
+* Python 3.7
+
+* Flask 1.1.1
--- basic/basic_app.py
+++ basic/basic_app.py
@@ -1,24 +1,26 @@
-from flask import Flask, render_template, request
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
 
-app = Flask(__name__)
+bp = Blueprint('basic', __name__, template_folder='templates', static_folder='static', url_prefix='/basic')
 
-# 自动生成的hello world代码
-@app.route('/')
+# hello world代码
+@bp.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World in Module '+__name__
 
 # 通过URL传递参数
-@app.route('/user/<name>')
+@bp.route('/user/<name>')
 def hello_user(name):
     return 'Hello {0}!'.format(name)
 
 # 利用模版显示变量名
-@app.route('/city/<city_name>')
+@bp.route('/city/<city_name>')
 def welcome(city_name):
     return render_template('city.html', name=city_name)
 
 # 表单传递参数，模版输出页面
-@app.route('/form', methods=['GET', 'POST'])
+@bp.route('form', methods=['GET', 'POST'])
 def form():
     name = None
     if request.method == 'POST' and 'name' in request.form:
@@ -26,38 +28,34 @@ def form():
     return render_template('form.html', name=name)
 
 # 添加CSS和javascript
-@app.route('/css_js')
+@bp.route('/css_js')
 def css_js():
     return render_template('css_js.html')
 
 # 遍历列表
-@app.route('/go_through_list')
+@bp.route('/go_through_list')
 def go_through_list():
     heroes = ["李白", "露娜", "韩信", "阿轲"]
     return render_template('go_through_list.html', heroes=heroes)
 
 # 遍历字典
-@app.route("/go_through_dict")
+@bp.route("/go_through_dict")
 def go_through_dict():
     hero = {"名字": "李白", "属性": "刺客", "铭文": "百穿"}
     return render_template("go_through_dict.html", info=hero)
 
 # 条件渲染
-@app.route("/condition/<hero>")
+@bp.route("/condition/<hero>")
 def condition(hero):
     return render_template("condition.html", hero=hero)
 
 # 过滤器的本质就是函数，用来修改变量的显示，格式化，运算等
-@app.route('/filters')
+@bp.route('/filters')
 def filters():
     return render_template('filters.html')
 
 # 定义自己的过滤器 my_reverse 返转列表
-@app.template_filter('my_reverse')
-def do_list_reverse(li):
-    li.reverse()
-    return li
-
-
-if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+# @bp.add_app_template_filter('my_reverse')
+# def do_list_reverse(li):
+#     li.reverse()
+#     return li
